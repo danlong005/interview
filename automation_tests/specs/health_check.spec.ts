095cb572cf34b52f16c32d5dae0e65136ca20e9d
Daniel Long <danlong5@gmail.com>
2022-11-07 14:17:55 -0600

    done

--- automation_tests/specs/health_check.spec.ts
+++ automation_tests/specs/health_check.spec.ts
@@ -1,7 +1,8 @@
 import chai, { expect } from 'chai';
+import mocha from 'mocha';
 
 describe("Health Check", () => {
     it("adds two numbers", () => {
-       expect(1+1).to.equal(2);
+        expect(1+1).to.equal(2);
     });
 });
