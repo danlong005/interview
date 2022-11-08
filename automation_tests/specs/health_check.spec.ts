098d848c254feed20c879dbb3e5d4d92f2105401
Daniel Long <danlong5@gmail.com>
2022-11-08 14:21:01 -0600

    notes

--- automation_tests/specs/health_check.spec.ts
+++ automation_tests/specs/health_check.spec.ts
@@ -1,6 +1,14 @@
+// @ts-ignore
 import chai, { expect } from 'chai';
+// @ts-ignore
 import mocha from 'mocha';
 import axios from 'axios';
+
+
+// NOTES:
+//
+// ENDPOINT: http://localhost:8080/v1/health
+//
 
 describe("Health Check", () => {
     it("adds two numbers", () => {
